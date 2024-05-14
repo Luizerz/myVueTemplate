a444d3480486216ab0bd72a0fe91c58ba5fd20a1
Luizerz <senaluiz1307@gmail.com>
2024-05-13 22:00:59 -0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const msg = ref()
+onMounted(() => {
+    msg.value = 'Hello World!'
+})
+</script>
+<template>
+    <h1>{{ msg + "!!!" }}</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      'primary-color': '#FF0000',
+      'secondary-color': '#00FF00'
+    },
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
+    }
+  },
+  plugins: []
+}
